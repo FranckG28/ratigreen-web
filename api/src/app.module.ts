36c6a63e84c641ac6bf7e678249d33d0fdf27e9b
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-10-30 13:54:50 +0100

    feat(api): add question module, service and controller

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuestionModule } from './question/question.module';
 
 @Module({
-  imports: [],
+  imports: [QuestionModule],
   controllers: [AppController],
   providers: [AppService],
 })
