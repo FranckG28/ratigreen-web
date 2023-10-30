3bbf906baebb74903e71ce4655feb92c9845f5d5
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-10-30 16:14:19 +0100

    feat: add prisma ORM and add config to use .env file

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestionModule } from './question/question.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [QuestionModule],
+  imports: [QuestionModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
