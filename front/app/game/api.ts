5208cf56c5a19aee08edfaf7cd42d3e5ae02a96b
Julien Von Der Marck <vdmjulien05@gmail.com>
2023-11-18 16:23:26 +0100

    feat: add mapper

--- front/app/game/api.ts
+++ front/app/game/api.ts
@@ -1,28 +1,10 @@
-import { Indicator } from "../models/indicator";
 import { Question } from "../models/question";
 
 export async function GET() : Promise<Question[]> {
     const res = await fetch('http://localhost:3000/api/questions')
-    const data = await res.json()
+    const data = await res.json() as Question[]
 
-    const questions: Question[] = data.map((question: any) => {
-        return {
-            id: question.id,
-            title: question.title,
-            answer: question.answer,
-            choices: question.choices.map((choice: any) => ({
-                id: choice.id,
-                text: choice.text,
-                questionId: choice.questionId,
-                indicatorCoefficients: choice.indicatorCoefficients.map((ic: any) => ({
-                    id: ic.id,
-                    coefficient: ic.coefficient,
-                    choiceId: ic.choiceId,
-                    indicator: ic.indicator as Indicator,
-                })),
-            })),
-        };
-    })
+    //const questions: Question[] = data.map(mapQuestion)
 
-    return questions; 
+    return data; 
 }
