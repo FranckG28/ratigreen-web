c40f5d233f8d5db05b1e65fbb6130443037670ca
yanovskyy <alexander.yanovs-kyy@etu.unistra.fr>
2023-12-08 01:58:29 +0100

    trying to fix build

--- app/admin/components/ChoiceInput.tsx
+++ app/admin/components/ChoiceInput.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import Image from 'next/image';
-import { getIndicatorImage } from '@/app/utility/indicator-image-path';
-
 export interface ChoiceInputProps {
-    indexChoice: number;
-    isAnswer: boolean;
-    choiceTitle: string;
-    onRadioClickIsAnswer: () => void;
+  indexChoice: number;
+  isAnswer: boolean;
+  choiceTitle: string;
+  onRadioClickIsAnswer: () => void;
 }
 
 export default function ChoiceInput({ indexChoice, isAnswer, onRadioClickIsAnswer, choiceTitle }: ChoiceInputProps) {
@@ -18,18 +15,18 @@ export default function ChoiceInput({ indexChoice, isAnswer, onRadioClickIsAnswe
           <h2 className='text-lg opacity-80 font-bold uppercase'>Réponse</h2>
           <textarea style={{ resize: 'none' }} name={`choices[${indexChoice}].text`} className='rounded-2xl bg-white h-24 p-4 text-black w-full' required defaultValue="Oui, c'est une maladie grave." ></textarea>
         </div>
-        
+
         <div>
           <h2 className='text-lg opacity-80 font-bold uppercase'>Effets sur les indicateurs</h2>
           <div className='mt-2 flex lg:gap-12 flex-col lg:flex-row'>
-            
+
           </div>
         </div>
 
         <div className='flex gap-5 items-center'>
-          <h2 className='text-lg opacity-80 font-bold uppercase'>Est-ce la bonne réponse ?</h2> 
+          <h2 className='text-lg opacity-80 font-bold uppercase'>Est-ce la bonne réponse ?</h2>
           <input type='radio' checked={isAnswer} readOnly onClick={() => onRadioClickIsAnswer()} className='w-6 h-6'></input>
-        </div> 
+        </div>
       </div>
     </div>
   )
