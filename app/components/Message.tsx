64bb21d3ad5c42c9a5d978e5b52e28a56e8c5d9d
Raffael Di Pietro <raffael.dpietro@pixacare.com>
2023-12-08 08:04:34 +0100

    feat(messages): improvmenets

--- app/components/Message.tsx
+++ app/components/Message.tsx
@@ -10,25 +10,39 @@ const messagesMock = [
   "Heeey !",
   "Aaah je comprend mieux",
   "C'est vrai ???",
+  "C'est faux ??? Maman ne m'a pas dit ça.",
+  "Je vais le dire à mon Papa",
+  "Je ne me suis pas levé pour rien ce matin",
+  "Je ne savais pas ça",
+  "je me coucherais moins bête ce soir",
 ];
 
 export default function Message({ question }: { question: Question }) {
   return (
     <div className="overflow-auto bg-white rounded-xl h-1/3 justify-center shadow shadow-primary/50">
       <div className="text-primary-content text-lg font text-center p-10">
-        {
-          <div className="chat chat-end">
-            <div className="chat-bubble">
-              {
-                messagesMock[
-                  Math.floor(
-                    Math.random() * (messagesMock.length - 1 - 0 + 1) + 0
-                  )
-                ]
-              }
-            </div>
+        <div className="chat chat-end">
+          <div className="chat-bubble">
+            {
+              messagesMock[
+                Math.floor(
+                  Math.random() * (messagesMock.length - 1 - 0 + 1) + 0
+                )
+              ]
+            }
           </div>
-        }
+        </div>
+        <div className="chat chat-start">
+          <div className="chat-bubble">
+            {
+              messagesMock[
+                Math.floor(
+                  Math.random() * (messagesMock.length - 1 - 0 + 1) + 0
+                )
+              ]
+            }
+          </div>
+        </div>
         {/* <InputMessage questionId={question.id} /> */}
       </div>
     </div>
